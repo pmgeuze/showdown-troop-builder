68fedd2ec0453df7dfcc5441ce1f04533fb7d7ec
Pieter-Michiel Geuze <pmgeuze@gmail.com>
2026-02-24 20:05:29 -0500

    Update armor.js

--- js/armor.js
+++ js/armor.js
@@ -1,6 +1,8 @@
 var ARMORS = [
-    {name: 'Chain Mail',             armor: 2,  cost: 2},
-    {name: 'Hard Armor',             armor: 8,  cost: 8},
-    {name: 'Leather',                armor: 1,  cost: 1},
-    {name: 'Reinforced Leather',         armor: 2,  cost: 2},
+    {name: 'Breastplate', armor: 2,  cost: 2},
+    {name: 'Chain Mail', armor: 2,  cost: 2},
+    {name: 'Diving Helmet (Shark Man)', armor: 3,  cost: 3},
+    {name: 'Hard Armor', armor: 8,  cost: 8},
+    {name: 'Leather', armor: 1,  cost: 1},
+    {name: 'Reinforced Leather', armor: 2,  cost: 2},
 ];
